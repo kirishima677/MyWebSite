47d85049c04246bfafdc82e199f565d788d2678c
kirishima677 <nishioka.temp@gmail.com>
2021-10-07 13:40:04 +0900

    記事の一覧表示のパジネーターを実装

--- app/lib/util/templates_util/template_util.go
+++ app/lib/util/templates_util/template_util.go
@@ -35,24 +35,45 @@ func GetTemplateFunctions() template.FuncMap {
  */
 func generatePaginatorLink(path string, rowCount int, offset int, limit int) template.HTML {
 
-	htmlString := "<nav aria-label=\"Page navigation example\">"
+	previousOffset := offset - limit
+	if previousOffset < 0 {
+		previousOffset = 0
+	}
 
+	nextOffset := offset + limit
+
+	htmlString := "<nav aria-label=\"Page navigation example\">"
 	htmlString = htmlString + "<ul class=\"pagination justify-content-center\">"
 
 	if offset == 0 {
 		htmlString = htmlString + "<li class=\"page-item disabled\">\n<a class=\"page-link\" href=\"#\" tabindex=\"-1\">Previous</a>\n</li>"
 	} else {
-		htmlString = htmlString + "<li class=\"page-item\">\n<a class=\"page-link\" href=\"#\" tabindex=\"-1\">Previous</a>\n</li>"
+		htmlString = htmlString + fmt.Sprintf("<li class=\"page-item\">\n<a class=\"page-link\" href=\"%s?offset=%d&limit=%d\" tabindex=\"-1\">Previous</a>\n</li>",
+			path, previousOffset, limit)
 	}
 
-	pageNumber := 1
-	htmlString = htmlString + fmt.Sprintf(""+
-		"<li class=\"page-item\">"+
-		"<a class=\"page-link\" href=\"%s?offset=%d&amp;limit=%d\">%d</a>"+
-		"</li>",
-		path, offset, limit, pageNumber)
+	pageNumber := int(math.Ceil(float64(rowCount / limit)))
+	nowPageNumber := int(math.Ceil(float64(offset/limit))) + 1
+	pageOffset := 0
+	for i := 0; i <= pageNumber; i++ {
+		disabled := ""
+		if (i + 1) == nowPageNumber {
+			disabled = "disabled"
+		}
 
-	htmlString = htmlString + "<li class=\"page-item\">\n<a class=\"page-link\" href=\"/admin/articles/list?offset=5&limit=5\">Next</a>\n</li>"
+		htmlString = htmlString + fmt.Sprintf(""+
+			"<li class=\"page-item %s\">"+
+			"<a class=\"page-link\" href=\"%s?offset=%d&amp;limit=%d\">%d</a>"+
+			"</li>",
+			disabled, path, pageOffset, limit, (i+1))
+		pageOffset = pageOffset + limit
+	}
+
+	if rowCount <= limit+offset {
+		htmlString = htmlString + "<li class=\"page-item disabled\">\n<a class=\"page-link\" href=\"#\">Next</a>\n</li>"
+	} else {
+		htmlString = htmlString + fmt.Sprintf("<li class=\"page-item\">\n<a class=\"page-link\" href=\"/admin/articles/list?offset=%d&limit=%d\">Next</a>\n</li>", nextOffset, limit)
+	}
 
 	htmlString = htmlString + "</ul>"
 	htmlString = htmlString + "</nav>"
